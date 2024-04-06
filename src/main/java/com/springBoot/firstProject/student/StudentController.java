fe860a98ebd4743a500d3fc87590ed2ce3b595ae
prathmesh8353 <prathmeshmali8353@gmail.com>
2024-04-06 20:51:51 +0530

    implementing dao layer

--- src/main/java/com/springBoot/firstProject/student/StudentController.java
+++ src/main/java/com/springBoot/firstProject/student/StudentController.java
@@ -1,8 +1,6 @@
 package com.springBoot.firstProject.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,8 +15,36 @@ public class StudentController {
         this.service = service;
     }
 
+    @PostMapping
+    public Student save (
+        @RequestBody Student student
+    ) {
+        return service.save(student);
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail (
+          @PathVariable("email")  String em
+    ) {
+        return service.findByEmail(em);
+    }
+
     @GetMapping
     public List<Student> findAllStudents() {
         return service.findAllStudents();
+    }
+
+    @PutMapping
+    public Student updateStudent (
+         @RequestBody Student student
+    ) {
+        return service.update(student);
+    }
+
+    @DeleteMapping("/{email}")
+    public void delete (
+         @PathVariable String email
+    ) {
+        service.delete(email);
     }
 }
--- src/main/java/com/springBoot/firstProject/student/StudentService.java
+++ src/main/java/com/springBoot/firstProject/student/StudentService.java
@@ -5,39 +5,15 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
 
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student(
-                        "Prathmesh",
-                        "Mali",
-                        LocalDate.now(),
-                        "prathmeshmali@gmail.com",
-                        24
-                ),
-                new Student(
-                        "Sudam",
-                        "Mali",
-                        LocalDate.now(),
-                        "prathmeshmali@gmail.com",
-                        59
-                ),
-                new Student(
-                        "Latika",
-                        "Mali",
-                        LocalDate.now(),
-                        "prathmeshmali@gmail.com",
-                        50
-                ),
-                new Student(
-                        "Susmita",
-                        "Mali",
-                        LocalDate.now(),
-                        "prathmeshmali@gmail.com",
-                        29
-                )
-        );
-    }
+public interface StudentService {
+
+    Student save (Student s);
+    List<Student> findAllStudents();
+
+    Student findByEmail (String email);
+
+    Student update(Student s);
+
+    void delete (String email);
 }
